27a99c80095407373813906729fed935448f3c18
tkaykim <tommy0621@naver.com>
2026-02-21 20:03:27 +0900

    rollback

--- android/app/src/main/java/com/moveit/app/MainActivity.java
+++ android/app/src/main/java/com/moveit/app/MainActivity.java
@@ -9,7 +9,6 @@ import android.view.ViewGroup;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import com.getcapacitor.Bridge;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -19,20 +18,6 @@ public class MainActivity extends BridgeActivity {
         super.onCreate(savedInstanceState);
         createNotificationChannel();
         applyNavigationBarInsets();
-        setupPaymentIntentWebViewClient();
-    }
-
-    /**
-     * 결제/금융 앱 스킴(intent://, ispmobile:// 등)을 가로채서
-     * 외부 브라우저가 아닌 해당 앱만 실행하도록 WebViewClient 교체.
-     */
-    private void setupPaymentIntentWebViewClient() {
-        runOnUiThread(() -> {
-            Bridge bridge = getBridge();
-            if (bridge != null) {
-                bridge.setWebViewClient(new PaymentIntentWebViewClient(bridge));
-            }
-        });
     }
 
     private void createNotificationChannel() {
